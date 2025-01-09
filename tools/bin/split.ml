e50c962af69fbba34944420c70b8e626ba077596
nojaf <florian.verdonck@outlook.com>
2025-01-09 22:23:25 +0100

    Process nested functions

--- tools/bin/split.ml
+++ tools/bin/split.ml
@@ -1,6 +1,26 @@
 [@@@warning "-27"]
 
-type nodeKind = Structure | ValueBinding | Pattern | Expression
+type nodeKind =
+  | Structure
+  | ValueBinding
+  | Pattern
+  | Expr
+  | ExprFun
+  | ExprArray
+  | ExprIdent
+  | ExprConstant
+  | ExprSeq
+
+let kind_to_string = function
+  | Structure -> "structure"
+  | ValueBinding -> "value_binding"
+  | Pattern -> "pattern"
+  | Expr -> "expr"
+  | ExprFun -> "expression_function"
+  | ExprArray -> "expression_array"
+  | ExprIdent -> "expression_ident"
+  | ExprConstant -> "expression_constant"
+  | ExprSeq -> "expression_sequence"
 
 type range = {startLine: int; startColumn: int; endLine: int; endColumn: int}
 
@@ -8,12 +28,13 @@ type node = {kind: nodeKind; range: range; children: node list}
 
 (*
 
-   dune exec rescript-tools -- split foo.res | bunx prettier --parser json
+   dune exec rescript-tools -- split A.res | bunx prettier --parser json --print-width 120
 
-   ./cli/bsc -dtypedtree -bs-ast A.res
+   ./cli/bsc -dtypedtree A.res
 *)
 
 let ( >> ) f g x = g (f x)
+let id x = x
 
 let loc_to_range (loc : Location.t) =
   {
@@ -26,8 +47,44 @@ let loc_to_range (loc : Location.t) =
 let mk_pattern (pat : Parsetree.pattern) : node =
   {kind = Pattern; range = loc_to_range pat.ppat_loc; children = []}
 
-let mk_expression (expr : Parsetree.expression) : node =
-  {kind = Expression; range = loc_to_range expr.pexp_loc; children = []}
+let rec mk_expression (expr : Parsetree.expression) : node =
+  let kind, children =
+    match expr.pexp_desc with
+    | Pexp_fun funExpr ->
+      let rec collect continuation (e : Parsetree.expression) : node list =
+        match e.pexp_desc with
+        | Pexp_fun fex ->
+          collect
+            (fun rest ->
+              let next =
+                match fex.default with
+                | None -> mk_pattern fex.lhs :: rest
+                | Some dex -> mk_pattern fex.lhs :: mk_expression dex :: rest
+              in
+              continuation next)
+            fex.rhs
+        | _ -> continuation [mk_expression e]
+      in
+      let children = collect id expr in
+      (ExprFun, children)
+    | Pexp_array arrayExpr -> (ExprArray, arrayExpr |> List.map mk_expression)
+    | Pexp_ident _ -> (ExprIdent, [])
+    | Pexp_constant _ -> (ExprConstant, [])
+    | Pexp_sequence (e1, e2) ->
+      (* Collect all nested sequences *)
+      let rec collect continuation (e : Parsetree.expression) :
+          Parsetree.expression list =
+        match e.pexp_desc with
+        | Pexp_sequence (e1, e2) ->
+          collect (fun rest -> e1 :: rest |> continuation) e2
+        | _ -> continuation [e]
+      in
+      let xs = e1 :: collect id e2 in
+      let nodes = List.map mk_expression xs in
+      (ExprSeq, nodes)
+    | _ -> (Expr, [])
+  in
+  {kind; range = loc_to_range expr.pexp_loc; children}
 
 let mk_attributes _ : node list = []
 
@@ -63,12 +120,6 @@ let mk_node (tree : Parsetree.structure_item) : node =
   let children = mk_structure_item_descr tree.pstr_desc in
   {kind = Structure; range; children}
 
-let kind_to_string = function
-  | Structure -> "structure"
-  | ValueBinding -> "value_binding"
-  | Pattern -> "pattern"
-  | Expression -> "expression"
-
 let range_to_json (range : range) : string =
   Printf.sprintf
     "{ \"startLine\": %d, \"startColumn\": %d, \"endLine\": %d, \"endColumn\": \
@@ -83,11 +134,11 @@ let rec node_to_json node =
   Printf.sprintf "{ \"kind\": \"%s\", \"range\": %s, \"children\": %s }"
     (kind_to_string node.kind) (range_to_json node.range) children
 
-let split (file : string) =
-  let source = "let a = () => 5" in
+let split (filename : string) =
+  let source = Res_io.read_file ~filename in
   let result =
     Res_driver.parse_implementation_from_source ~for_printer:false
-      ~display_filename:file ~source
+      ~display_filename:filename ~source
   in
   print_endline
     (result.parsetree
