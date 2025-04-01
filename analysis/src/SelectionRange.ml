f33cff73db3a36a8046a69d407944dca48d188a8
nojaf <florian.verdonck@outlook.com>
2025-04-01 21:58:47 +0200

    Use a Set to collect locations

--- analysis/src/SelectionRange.ml
+++ analysis/src/SelectionRange.ml
@@ -6,34 +6,49 @@ module SeenLocationSet = Set.Make (struct
   type t = Warnings.loc
 
   let compare (a : Warnings.loc) (b : Warnings.loc) =
-    match compare a.loc_start.pos_cnum b.loc_start.pos_cnum with
-    | 0 -> compare a.loc_end.pos_cnum b.loc_end.pos_cnum
-    | c -> c
+    if
+      a.loc_start.pos_cnum = b.loc_start.pos_cnum
+      && a.loc_end.pos_cnum = b.loc_end.pos_cnum
+    then 0
+    else
+      (* First compare by size *)
+      let sizeA = a.loc_end.pos_cnum - a.loc_start.pos_cnum in
+      let sizeB = b.loc_end.pos_cnum - b.loc_start.pos_cnum in
+      let sizeCompare = compare sizeA sizeB in
+      if sizeCompare <> 0 then sizeCompare
+      else
+        (* If sizes are equal, compare by position to ensure uniqueness *)
+        let startCompare = compare a.loc_start.pos_cnum b.loc_start.pos_cnum in
+        if startCompare <> 0 then startCompare
+        else compare a.loc_end.pos_cnum b.loc_end.pos_cnum
 end)
 
 let process_implementation (offset : int) (implementation : Parsetree.structure)
     : string =
-  let seen = ref SeenLocationSet.empty in
-  let locations = Stack.create () in
+  let locations = ref SeenLocationSet.empty in
 
   let location _iterator (loc : Warnings.loc) =
     if loc.loc_ghost then ()
-    else if SeenLocationSet.mem loc !seen then ()
+    else if SeenLocationSet.mem loc !locations then ()
     else
       let start = loc.loc_start in
       let end_ = loc.loc_end in
-      if start.pos_cnum <= offset && offset <= end_.pos_cnum then (
-        seen := SeenLocationSet.add loc !seen;
-        Stack.push loc locations)
+      if start.pos_cnum <= offset && offset <= end_.pos_cnum then
+        locations := SeenLocationSet.add loc !locations
   in
   let iterator = {Ast_iterator.default_iterator with location} in
   implementation |> List.iter (iterator.structure_item iterator);
 
-  locations
-  |> Stack.iter (fun loc ->
-         Format.printf "start: %d end: %d\n" loc.loc_start.pos_cnum
+  !locations |> SeenLocationSet.elements
+  |> List.iter (fun loc ->
+         Format.printf "start %d end %d\n" loc.loc_start.pos_cnum
            loc.loc_end.pos_cnum);
 
+  (* |> List.iter (fun loc ->
+         Format.printf "start: (%d, %d) end: (%d, %d)\n" loc.loc_start.pos_lnum
+           (loc.loc_start.pos_cnum - loc.loc_start.pos_bol)
+           loc.loc_end.pos_lnum
+           (loc.loc_end.pos_cnum - loc.loc_end.pos_bol)); *)
   Protocol.null
 
 (* 
@@ -52,13 +67,12 @@ let selectionRange ~(path : string) ~(line : int) ~(col : int) =
         Protocol.null
       | Some offset ->
         print_endline ("offset: " ^ string_of_int offset);
-        if Filename.check_suffix path ".res" then (
+        if Filename.check_suffix path ".res" then
           let parser =
             Res_driver.parsing_engine.parse_implementation ~for_printer:false
           in
-          print_endline "yow";
           let {Res_driver.parsetree = implementation} = parser ~filename:path in
-          process_implementation offset implementation)
+          process_implementation offset implementation
         else if Filename.check_suffix path ".resi" then
           let parser =
             Res_driver.parsing_engine.parse_interface ~for_printer:false
